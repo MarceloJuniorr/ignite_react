5fc8420a9f3889986ef45c4f4bfa36c462c2bfe8
Marcelo Junior <marcelo.junior@yes.technology>
2023-01-13 15:37:50 -0300

    Os motores do React - Propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,50 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [{
+
+  id: 1,
+  author: {
+    avatarUrl: "https://github.com/marcelojuniorr.png",
+    name: "Marcelo Junior",
+    role: "Frontend Developer"
+  },
+  publishedAt: new Date('2023-01-13 20:00:00'),
+  content:[
+    {type: 'paragraph', content: 'Fala galeraa 👋'},
+    {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+    {type: 'link', content: 'jane.design/doctorcare' }
+  ]
+},
+{
+  id: 2,
+  author: {
+    avatarUrl: "https://github.com/maykbrito.png",
+    name: "Mayk Brito",
+    role: "Educator"
+  },
+  publishedAt: new Date('2023-01-03 20:00:00'),
+  content:[
+    {type: 'paragraph', content: 'Fala galeraa 👋'},
+    {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+    {type: 'link', content: 'jane.design/doctorcare' }
+  ]
+},
+{
+  id: 3,
+  author: {
+    avatarUrl: "https://github.com/diego3g.png",
+    name: "Diego Fernandes",
+    role: "CTO Rocketseat"
+  },
+  publishedAt: new Date('2023-01-12 20:00:00'),
+  content:[
+    {type: 'paragraph', content: 'Fala galeraa 👋'},
+    {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+    {type: 'link', content: 'jane.design/doctorcare' }
+  ]
+}]
+
 export function App() {
   return (
     <div>
@@ -14,14 +58,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Diego Fernandes"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-          />
-          <Post
-            author="Gabriel Buzzi"
-            content="Um novo post muito legal"
-          />
+          {posts.map(post => {
+           return  (
+            <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+           
+            />
+           )
+          })}
         </main>
       </div>
     </div>
