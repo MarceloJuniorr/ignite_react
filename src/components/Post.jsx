e1eb959ecae86e279666a88a74a278d8a7e3154f
Marcelo Junior <marcelo.junior@yes.technology>
2023-01-13 16:35:33 -0300

    Os motores do React - Propriedades do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,6 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
@@ -5,22 +8,38 @@ import styles from './Post.module.css';
 
 
 
-export function Post() {
+export function Post({author, content, publishedAt}) {
+  const publishedDateFormatted = format(publishedAt,"d 'de' LLLL 'às' HH:mm'h'",{locale: ptBR} )
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/marcelojuniorr.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Marcelo Junior</strong>
-            <span>Frontend Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>
+          }
+          if (line.type === 'link') {
+            return <p><a href="">{line.content}</a></p>
+          }
+        })}
 
 
       </div>
