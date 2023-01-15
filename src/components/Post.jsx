4637cff67b6d2629b5509703f25366a4e14466ba
Marcelo Junior <marcelo.junior@yes.technology>
2023-01-15 18:51:30 -0300

    Os motores do React - Entendendo a key

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -53,10 +53,10 @@ function handleNewCommentChange() {
       <div className={styles.content}>
         {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           }
           if (line.type === 'link') {
-            return <p><a href="">{line.content}</a></p>
+            return <p key={line.content}><a href="">{line.content}</a></p>
           }
         })}
 
@@ -80,7 +80,7 @@ function handleNewCommentChange() {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment content={comment}/>
+          return <Comment key={comment} content={comment}/>
         })}
       </div>
     </article>
